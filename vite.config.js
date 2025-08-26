850157a617b84c828be3f1da69376c6218b45f22
noname121222 <sulajmonmuhamedov6@gmail.com>
2025-08-26 11:52:11 +0500

    fix vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(
+  plugins: [
+    react(),
     tailwindcss(),
-  )],
+  ],
+  base: '/wwww/', // имя твоего репозитория на GitHub
 })
