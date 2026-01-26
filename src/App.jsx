9f6e1f557d696ec42731102ece90875e5c204f21
noname121222 <sulajmonmuhamedov6@gmail.com>
2026-01-26 22:15:31 +0500

    qoshdim

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,25 @@
 import { useState } from 'react'
-
-import {First,Second,Third,Fourth} from './File'
-import Layout from "./Layout";
-import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from 'react-router-dom'
-
+import { First, Second, Third, Fourth } from './File'
+import Layout from "./Layout"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element ={<Layout/>} >
-        <Route index  element ={<First/>}/> 
-        <Route path='/second'  element ={<Second/>}/> 
-        <Route path='/third'  element ={<Third/>}/> 
-        <Route path='/fourth'  element ={<Fourth/>}/> 
-
-        
+      <Route 
+        path="/" 
+        element={<Layout />} 
+        errorElement={<h1 className="text-center text-red-500 mt-10">Страница не найдена</h1>} 
+      >
+        <Route index element={<First />} /> 
+        <Route path="second" element={<Second />} /> 
+        <Route path="third" element={<Third />} /> 
+        <Route path="fourth" element={<Fourth />} /> 
       </Route>
     )
   )
-  
 
-  return (
-    
-    
-  
-      <>
-      
-      
-      <RouterProvider  router={router}/>
-            
-  
-      </>
-   
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(
     tailwindcss(),
-  ],
-  base: '/wwww/', // имя твоего репозитория на GitHub
+  )],
 })
